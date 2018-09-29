790a2248d400aaf8e9e83a7cfe48f5b6acf1b14f
CodeWinner <ngokhacbac18695@gmail.com>
2018-09-29 15:53:38 +0700

    Create model

---
+++ app/src/main/java/com/studio/bin/managemoney/model/ActionDay.java
@@ -0,0 +1,54 @@
+package com.studio.bin.managemoney.model;
+
+import java.util.Date;
+
+public class ActionDay {
+    private int idAction;
+    private int idWorkOfMounth;
+    private double moneyGoalOfDay;
+    private Date actionDate;
+    private int status;
+
+    public ActionDay() {
+    }
+
+    public int getIdAction() {
+        return idAction;
+    }
+
+    public void setIdAction(int idAction) {
+        this.idAction = idAction;
+    }
+
+    public int getIdWorkOfMounth() {
+        return idWorkOfMounth;
+    }
+
+    public void setIdWorkOfMounth(int idWorkOfMounth) {
+        this.idWorkOfMounth = idWorkOfMounth;
+    }
+
+    public double getMoneyGoalOfDay() {
+        return moneyGoalOfDay;
+    }
+
+    public void setMoneyGoalOfDay(double moneyGoalOfDay) {
+        this.moneyGoalOfDay = moneyGoalOfDay;
+    }
+
+    public Date getActionDate() {
+        return actionDate;
+    }
+
+    public void setActionDate(Date actionDate) {
+        this.actionDate = actionDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
---
+++ app/src/main/java/com/studio/bin/managemoney/model/WorkOfMounth.java
@@ -0,0 +1,57 @@
+package com.studio.bin.managemoney.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class WorkOfMounth {
+    private int idWork;
+    private int workName;
+    private double goalMoneyOfWork;
+    private Date mounthOfWork;
+    private List<ActionDay> actionDateds;
+
+    /**
+     * Contructor
+     * */
+    public WorkOfMounth(){}
+
+    public List<ActionDay> getActionDateds() {
+        return actionDateds;
+    }
+
+    public void setActionDateds(List<ActionDay> actionDateds) {
+        this.actionDateds = actionDateds;
+    }
+
+    public int getIdWork() {
+        return idWork;
+    }
+
+    public void setIdWork(int idWork) {
+        this.idWork = idWork;
+    }
+
+    public int getWorkName() {
+        return workName;
+    }
+
+    public void setWorkName(int workName) {
+        this.workName = workName;
+    }
+
+    public double getGoalMoneyOfWork() {
+        return goalMoneyOfWork;
+    }
+
+    public void setGoalMoneyOfWork(double goalMoneyOfWork) {
+        this.goalMoneyOfWork = goalMoneyOfWork;
+    }
+
+    public Date getMounthOfWork() {
+        return mounthOfWork;
+    }
+
+    public void setMounthOfWork(Date mounthOfWork) {
+        this.mounthOfWork = mounthOfWork;
+    }
+}
